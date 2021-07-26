adceaad1e6d54cc4ee89f79c34d017d209304dee
TONY <supawat125947@gmail.com>
2021-07-26 23:17:28 +0700

    plot 1-3-freq

--- bjt-amp-lab/CB-freq.py
+++ bjt-amp-lab/CB-freq.py
@@ -4,12 +4,11 @@ from matplotlib.ticker import EngFormatter
 
 
 def main():
-    # read rawdata
-    filename = '1-1.raw'
+    filename = '1-3(2nd).raw'
     rawdata = Ltspice(filename)
     rawdata.parse()
+    print(rawdata.get_data('V(vout)'))
 
-    # prepare data
     raw_vout = np.absolute(rawdata.get_data('V(vout)'))
     vout = []
     for data in raw_vout:
@@ -18,10 +17,9 @@ def main():
     vout = np.array(vout)
     freq = rawdata.get_frequency()
 
-    # vitualize data
     figure, axis1 = plt.subplots(figsize=(8, 4))
     axis1.set_xscale('log')
-    axis1.set_title('CE Amplifier Frequency Characteristic')
+    axis1.set_title('CC Amplifier Frequency Characteristic')
     formatter1 = EngFormatter(unit='Hz')
     formatter2 = EngFormatter(unit='dB')
     axis1.xaxis.set_major_formatter(formatter1)
@@ -30,10 +28,9 @@ def main():
     axis1.set_xlabel('Frequency')
     axis1.set_ylabel('Amplifier Gain')
     plt.autoscale(enable=True, axis='x', tight=True)
-    axis1.xaxis.grid(True, which="minor", ls="dotted", color='lightgrey')
-    axis1.grid(True, which="major", ls="dashed", color='grey')
+    axis1.xaxis.grid(True, which='minor', ls='dotted', color='lightgrey')
+    axis1.grid(True, which='major', ls='dashed', color='grey')
 
-    # display data
     plt.tight_layout()
     plt.savefig(f'{filename[:-4]}-freq.png')
     plt.show()
