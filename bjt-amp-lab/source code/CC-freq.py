008c0eb33bfc3ad3b3a8cec1c7a3ce8400c255c2
TONY <supawat125947@gmail.com>
2021-07-27 14:15:34 +0700

    plot CC freq. res.

--- bjt-amp-lab/source code/CC-freq.py
+++ bjt-amp-lab/source code/CC-freq.py
@@ -4,7 +4,35 @@ from matplotlib.ticker import EngFormatter
 
 
 def main():
-    pass
+    filename = './ltspice/1-2.raw'
+    rawdata = Ltspice(filename)
+    rawdata.parse()
+
+    raw_vout = np.absolute(rawdata.get_data('V(vout)'))
+    vout = []
+    for data in raw_vout:
+        vout.append(20 * np.log10(data))
+
+    vout = np.array(vout)
+    freq = rawdata.get_frequency()
+
+    figure, axis1 = plt.subplots(figsize=(8, 4))
+    axis1.set_xscale('log')
+    axis1.set_title('CC Amplifier Frequency Characteristic')
+    formatter1 = EngFormatter(unit='Hz')
+    formatter2 = EngFormatter(unit='dB')
+    axis1.xaxis.set_major_formatter(formatter1)
+    axis1.yaxis.set_major_formatter(formatter2)
+    axis1.plot(freq, vout)
+    axis1.set_xlabel('Frequency')
+    axis1.set_ylabel('Amplifier Gain')
+    plt.autoscale(enable=True, axis='x', tight=True)
+    axis1.xaxis.grid(True, which='minor', ls='dotted', color='lightgrey')
+    axis1.grid(True, which='major', ls='dashed', color='grey')
+
+    plt.tight_layout()
+    plt.savefig(f'./figure/{filename[:-4]}-freq.png')
+    plt.show()
 
 
 if __name__ == '__main__':
